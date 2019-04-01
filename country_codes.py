9403265eed97b00eabe55c10eda01d8216012ed1
X <ftha@qq.com>
2019-04-01 08:24:39 +0800

    enter

--- country_codes.py
+++ country_codes.py
@@ -6,5 +6,8 @@ def get_country_code(country_name):
     for code, name in COUNTRIES.items():
         if name == country_name:
             return code
+        elif country_name == 'Yemen, Rep':
+            return 'ye'
+
     # 如果没有找到指定的国家，就返回None
     return None
